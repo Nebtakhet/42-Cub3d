c8be5a41b2dd356fa398ba422501e686f9fd560f
Nuutti Vallin <nvallin@c1r5p18.hive.fi>
2024-10-21 12:47:40 +0300

    combining cast ray with fov

--- minimap_ray.c
+++ minimap_ray.c
@@ -50,30 +50,30 @@ int	ft_x_ray(double direction, double *x_draw)
 	return (0);
 }
 
-static void draw_fov(t_data *data)
-{
-	double	x;
-	double	y;
-	double	angle;
-	double	dir_x;
-	double	dir_y;
+// static void draw_fov(t_data *data)
+// {
+// 	double	x;
+// 	double	y;
+// 	double	angle;
+// 	double	dir_x;
+// 	double	dir_y;
 
-	angle = data->player.angle - (FOV / 2);
-	while (angle < data->player.angle + (FOV / 2))
-	{
-		x = data->player.mini_player->instances[0].x + 8;
-		y = data->player.mini_player->instances[0].y + 8;
-		dir_x = cos(angle);
-		dir_y = sin(angle);
-		while (data->map[(int)y / 16][(int)x / 16] != '1')
-		{
-			mlx_put_pixel(data->img, (int)x, (int)y, data->palette[12]);
-			y += dir_y * 0.1;
-			x += dir_x * 0.1;
-		}
-		angle += 0.01;
-	}
-}
+// 	angle = data->player.angle - (FOV / 2);
+// 	while (angle < data->player.angle + (FOV / 2))
+// 	{
+// 		x = data->player.mini_player->instances[0].x + 8;
+// 		y = data->player.mini_player->instances[0].y + 8;
+// 		dir_x = cos(angle);
+// 		dir_y = sin(angle);
+// 		while (data->map[(int)y / 16][(int)x / 16] != '1')
+// 		{
+// 			mlx_put_pixel(data->img, (int)x, (int)y, data->palette[12]);
+// 			y += dir_y * 0.1;
+// 			x += dir_x * 0.1;
+// 		}
+// 		angle += 0.01;
+// 	}
+// }
 
 void	ft_draw_ray(t_data *data, int color)
 {
@@ -81,16 +81,25 @@ void	ft_draw_ray(t_data *data, int color)
 	int	y;
 	double	x_draw;
 	double	y_draw;
+	double	angle;
+	double	dir_x;
+	double	dir_y;	
 
-	x_draw = data->player.mini_player->instances[0].x + 8;
-	y_draw = data->player.mini_player->instances[0].y + 8;
-	x = data->player_pos[1];
-	y = data->player_pos[0];
-	while (data->map[y][x] != '1')
+	angle = data->player.angle - (FOV / 2);
+	while (angle < data->player.angle + (FOV / 2))
 	{
-		mlx_put_pixel(data->img, x_draw, y_draw, color);
-		y += ft_y_ray(data->player.dir_y, &y_draw);
-		x += ft_x_ray(data->player.dir_x, &x_draw);
+		x_draw = data->player.mini_player->instances[0].x + 8;
+		y_draw = data->player.mini_player->instances[0].y + 8;
+		x = data->player_pos[1];
+		y = data->player_pos[0];
+		dir_x = cos(angle);
+		dir_y = sin(angle);
+		while (data->map[y][x] != '1')
+		{
+			mlx_put_pixel(data->img, x_draw, y_draw, color);
+			y += ft_y_ray(dir_y, &y_draw);
+			x += ft_x_ray(dir_x, &x_draw);
+		}
+		angle += 0.01;
 	}
-	draw_fov(data);
 }
