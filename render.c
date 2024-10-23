190d4800a2f95b9f56a5721710609f47f39bd20e
Cesar Sanchez <cesasanc@c1r6p14.hive.fi>
2024-10-23 13:09:52 +0300

    Added shades for distance

--- render.c
+++ render.c
@@ -6,11 +6,41 @@
 /*   By: cesasanc <cesasanc@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 14:24:13 by cesasanc          #+#    #+#             */
-/*   Updated: 2024/10/23 11:29:09 by cesasanc         ###   ########.fr       */
+/*   Updated: 2024/10/23 13:08:20 by cesasanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Cub3d.h"
+
+static int apply_lighting(int color, double lighting_factor)
+{
+    int r;
+	int g;
+	int b;
+	int a;
+
+    r = (color >> 24) & 0xFF;
+    g = (color >> 16) & 0xFF;
+    b = (color >> 8) & 0xFF;
+    a = color & 0xFF;
+    r = (int)(r * lighting_factor);
+    g = (int)(g * lighting_factor);
+    b = (int)(b * lighting_factor);
+    if (r > 255)
+		r = 255;
+    if (r < 0)
+		r = 0;
+    if (g > 255)
+		g = 255;
+    if (g < 0)
+		g = 0;
+    if (b > 255)
+		b = 255;
+    if (b < 0)
+		b = 0;
+    return (get_rgba(r, g, b, a));
+}
+
 
 /* Function to calculate the image to be rendered, placing the corresponding
 pixel color in the image */
@@ -18,6 +48,7 @@ void	calculate_img(t_data *data)
 {
 	t_img	img;
 	int		color;
+	double	lightning_factor;
 
 	img.x = 0;
 	while (img.x < WIDTH)
@@ -27,6 +58,8 @@ void	calculate_img(t_data *data)
 			color = get_rgba(0, 255, 0, 255);
 		else
 			color = get_rgba(0, 0, 255, 255);
+		lightning_factor = 1.0 / (1.0 + data->ray.perp_wall_dist * 0.6);
+		color = apply_lighting(color, lightning_factor);
 		img.y = 0;
 		while (img.y < HEIGHT)
 		{
