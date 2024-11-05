6dfb7cd7033ed7e2efb83ccd332411a503f8bef8
César <cesasanc@student.hive.fi>
2024-11-05 12:02:45 +0200

    Added delay on doors

--- doors_bonus.c
+++ doors_bonus.c
@@ -6,7 +6,7 @@
 /*   By: cesasanc <cesasanc@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 12:14:56 by cesasanc          #+#    #+#             */
-/*   Updated: 2024/11/04 13:54:37 by cesasanc         ###   ########.fr       */
+/*   Updated: 2024/11/05 11:53:27 by cesasanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,24 +32,20 @@ int	ft_is_near_door(t_data *data, char axis, int direction)
 	x = data->player.pos_x;
 	if (axis == 'y')
 	{
-		if (direction > 0 && data->map[(int)(y + 0.5)][(int)x] == 'D')
+		if ((direction > 0 && data->map[(int)(y + 0.5)][(int)x] == 'D')
+			|| (direction < 0 && data->map[(int)(y - 0.5)][(int)x] == 'D'))
 			return (1);
-		if (direction > 0 && data->map[(int)(y + 0.5)][(int)x] == 'd')
-			return (2);
-		if (direction < 0 && data->map[(int)(y - 0.5)][(int)x] == 'D')
-			return (1);
-		if (direction < 0 && data->map[(int)(y - 0.5)][(int)x] == 'd')
+		if ((direction > 0 && data->map[(int)(y + 0.5)][(int)x] == 'd')
+			|| (direction < 0 && data->map[(int)(y - 0.5)][(int)x] == 'd'))
 			return (2);
 	}
 	if (axis == 'x')
 	{
-		if (direction > 0 && data->map[(int)y][(int)(x + 0.5)] == 'D')
+		if ((direction > 0 && data->map[(int)y][(int)(x + 0.5)] == 'D')
+			|| (direction < 0 && data->map[(int)y][(int)(x - 0.5)] == 'D'))
 			return (1);
-		if (direction > 0 && data->map[(int)y][(int)(x + 0.5)] == 'd')
-			return (2);
-		if (direction < 0 && data->map[(int)y][(int)(x - 0.5)] == 'D')
-			return (1);
-		if (direction < 0 && data->map[(int)y][(int)(x - 0.5)] == 'd')
+		if ((direction > 0 && data->map[(int)y][(int)(x + 0.5)] == 'd')
+			|| (direction < 0 && data->map[(int)y][(int)(x - 0.5)] == 'd'))
 			return (2);
 	}
 	return (0);
@@ -57,7 +53,6 @@ int	ft_is_near_door(t_data *data, char axis, int direction)
 
 static bool	valid_door_position(t_data *data, int x, int y)
 {
-
 	if (x > 0 && x < data->map_width - 1 && y > 0 && y < data->map_height - 1)
 	{
 		if ((data->map[y][x - 1] == '1' && data->map[y][x + 1] == '1') &&
@@ -88,10 +83,7 @@ void	place_doors(t_data *data)
 		while (x < data->map_width - 1)
 		{
 			if (valid_door_position(data, x, y))
-			{
 				data->map[y][x] = 'D';
-				printf("Door placed at (%d, %d)\n", x, y);
-			}
 			x++;
 		}
 		y++;
@@ -100,30 +92,28 @@ void	place_doors(t_data *data)
 
 void	door_interaction(t_data *data)
 {
-	int	door_y;
-	int	door_x;
+	int			door_y;
+	int			door_x;
+	static int	door_timer = 0;
 
-	if (ft_is_near_door(data, 'y', 1) || ft_is_near_door(data, 'y', -1)
-		|| ft_is_near_door(data, 'x', 1) || ft_is_near_door(data, 'x', -1))
+	if (door_timer <= 0)
 	{
-		door_y = (int)(data->player.pos_y + data->player.dir_y * 0.6);
-		door_x = (int)(data->player.pos_x + data->player.dir_x * 0.6);
-
-		if (data->map[door_y][door_x] == 'D')
+		if (ft_is_near_door(data, 'y', 1) || ft_is_near_door(data, 'y', -1)
+			|| ft_is_near_door(data, 'x', 1) || ft_is_near_door(data, 'x', -1))
 		{
-			data->map[door_y][door_x] = 'd';
-			printf("Door opened at (%d, %d)\n", door_x, door_y);
-			int	i = 0;
-			while (data->map[i] != NULL)
-				printf("%s\n", data->map[i++]);
+			door_y = (int)(data->player.pos_y + data->player.dir_y * 0.6);
+			door_x = (int)(data->player.pos_x + data->player.dir_x * 0.6);
+			if (data->map[door_y][door_x] == 'D')
+				data->map[door_y][door_x] = 'd';
+			else if (data->map[door_y][door_x] == 'd')
+				data->map[door_y][door_x] = 'D';
+			data->renderer.changed = true;
+			door_timer = 1;
 		}
-		else if (data->map[door_y][door_x] == 'd')
-		{
-			data->map[door_y][door_x] = 'D';
-			printf("Door closed at (%d, %d)\n", door_x, door_y);
-		}
+	}
+	else
+	{
+		door_timer--;
 		data->renderer.changed = true;
 	}
 }
-
-
