9b49b9a5804c6c4cc3c90412db406bed3ca8c649
Cesar Sanchez <cesasanc@c1r5p1.hive.fi>
2024-11-07 15:38:45 +0200

    Added enemy utils

---
+++ enemy_utils_bonus.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   enemy_utils_bonus.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cesasanc <cesasanc@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/07 15:17:08 by cesasanc          #+#    #+#             */
+/*   Updated: 2024/11/07 15:33:52 by cesasanc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Cub3d.h"
+
+void	you_win(t_data *data)
+{
+	if (!data->enemy_count && !data->you_win)
+	{
+		data->you_win = mlx_put_string(data->mlx, "YOU WIN!",	WIDTH / 2.5, HEIGHT / 2.5);
+		mlx_resize_image(data->you_win, 500, 200);
+	}
+}
